4460f8f57331e3daa9a71cef0ff30e33b54be4c5
a.dziameshchyk <alexanderdemeshchik@outlookc.om>
2017-12-01 17:42:54 +0300

    Add project files.

---
+++ Lab3/Context/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using Lab3.Models;
+using System.Data.Entity;
+
+namespace Lab3.Context
+{
+    public class AppContext: DbContext
+    {
+        public AppContext() : base("DefaultConnection") { }
+        public DbSet<Student> Students { get; set; }
+    }
+}
